c86066046214efa5476d52d58b6be38a941a7aa9
joaomenna1 <joaomenna1@gmail.com>
2025-11-05 02:34:19 -0400

    web integrations from api

--- web/src/components/webhooks-list.tsx
+++ web/src/components/webhooks-list.tsx
@@ -1,9 +1,25 @@
+import { useQuery } from '@tanstack/react-query'
 import { WebhooksListItem } from './webhooks-list-item'
+import { webhookListSchema } from '../http/schemas/webhooks'
 
 export function WebhooksList() {
+  const { data } = useQuery({
+    queryKey: ['webhooks'],
+    queryFn: async() => {
+      const response = await fetch('http://localhost:3333/api/webhooks')
+      const data = await response.json()
+
+      return webhookListSchema.parse(data)
+    },
+  })
+
+
   return (
     <div className="flex-1 overflow-auto">
       <div className="space-y-1 p-2">
+        <pre>
+            {JSON.stringify(data)}
+        </pre>
         <WebhooksListItem />
         <WebhooksListItem />
         <WebhooksListItem />
